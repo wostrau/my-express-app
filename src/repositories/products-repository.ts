5c879800b2c059167612aeaea33f2cf2b98069eb
wostrau <a.ostrovtsov@gmail.com>
2023-04-26 20:07:56 +0200

    04 / Data Access Layer + Repositories implementation - DONE

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -4,9 +4,37 @@ const products = [
 ]
 
 export const productsRepository = {
-    findProducts(searchTerm: string | null) {
-        if (searchTerm) {
-            return products.filter(p => p.title.indexOf(searchTerm) > -1)
+    findProducts(title: string | null) {
+        if (title) {
+            return products.filter(p => p.title.indexOf(title) > -1)
         } else return products
+    },
+    findProductById(id: number) {
+        const searchedProduct = products.find(p => p.id === id)
+        return searchedProduct
+    },
+    createProduct(title: string) {
+        const newProduct = {
+            id: Number(new Date()),
+            title: title
+        }
+        products.push(newProduct)
+        return newProduct
+    },
+    updateProduct(id: number, title: string) {
+        const updatedProduct = products.find(p => p.id === id)
+        if (updatedProduct) {
+            updatedProduct.title = title
+            return true
+        } else return false
+    },
+    deleteProduct(id: number) {
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].id === id) {
+                products.splice(i, 1)
+                return true
+            }
+        }
+        return false
     }
 }
--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -5,7 +5,7 @@ export const productsRouter = Router({})
 
 productsRouter.get('/', (req: Request, res: Response) => {
     const title = typeof req.query.title === 'string'
-        ? req.query.title
+        ? req.query.title?.toString()
         : null
 
     const foundProducts = productsRepository.findProducts(title)
@@ -15,32 +15,26 @@ productsRouter.get('/', (req: Request, res: Response) => {
 })
 
 productsRouter.get('/:id', (req: Request, res: Response) => {
-    const product = products.find(p => p.id === Number(req.params.id))
+    const product = productsRepository.findProductById(Number(req.params.id))
     if (product) res.send(product)
     else res.send(404)
 })
 
 productsRouter.delete('/:id', (req: Request, res: Response) => {
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id === Number(req.params.id)) {
-            products.splice(i, 1)
-            res.send(204)
-            return
-        }
-    }
-    res.send(404)
+    const isDeleted = productsRepository.deleteProduct(Number(req.params.id))
+    if (isDeleted) res.send(204)
+    else res.send(404)
 })
 
 productsRouter.post('/', (req: Request, res: Response) => {
-    const newProduct = {id: Number(new Date()), title: req.body.title}
-    products.push(newProduct)
+    const newProduct = productsRepository.createProduct(req.body.title)
     res.status(201).send(newProduct)
 })
 
 productsRouter.put('/:id', (req: Request, res: Response) => {
-    const updatedProduct = products.find(p => p.id === Number(req.params.id))
-    if (updatedProduct) {
-        updatedProduct.title = req.body.title
-        res.status(201).send(updatedProduct)
+    const isUpdated = productsRepository.updateProduct(Number(req.params.id), req.body.title)
+    if (isUpdated) {
+        const product = productsRepository.findProductById(Number(req.params.id))
+        res.send(product)
     } else res.send(404)
 })
