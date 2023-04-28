8a83a38737892495c054a2dbeab4d0fb057cea24
wostrau <a.ostrovtsov@gmail.com>
2023-04-28 15:03:53 +0200

    06 / Validation with new library express-validation - DONE

--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -2,8 +2,15 @@ import {Request, Response, Router} from 'express'
 import {productsRepository} from '../repositories/products-repository'
 import {requestCounter} from '../index'
 import {body, validationResult} from 'express-validator'
+import {inputValidationMiddleware} from '../middlewares/input-validation-middleware'
 
 export const productsRouter = Router({})
+
+const titleValidation = body('title')
+    .trim()
+    .isLength({min: 3, max: 15})
+    .withMessage('title length should be from 3 to 10 symbols')
+
 
 productsRouter.get('/', (req: Request, res: Response) => {
     const title = typeof req.query.title === 'string'
@@ -18,37 +25,35 @@ productsRouter.get('/', (req: Request, res: Response) => {
 
 productsRouter.get('/:id', (req: Request, res: Response) => {
     const product = productsRepository.findProductById(Number(req.params.id))
+
     if (product) res.send(product)
     else res.send(404)
 })
 
 productsRouter.delete('/:id', (req: Request, res: Response) => {
     const isDeleted = productsRepository.deleteProduct(Number(req.params.id))
+
     if (isDeleted) res.send(204)
     else res.send(404)
 })
 
 productsRouter.post('/',
-    body('title')
-        .isLength({min: 3, max: 15})
-        .withMessage('title length should be from 3 to 10 symbols'),
+    titleValidation,
+    inputValidationMiddleware,
     (req: Request, res: Response) => {
-
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            res.status(400).json({
-                errors: errors.array()
-            })
-        }
-
         const newProduct = productsRepository.createProduct(req.body.title)
+
         res.status(201).send(newProduct)
     })
 
-productsRouter.put('/:id', (req: Request, res: Response) => {
-    const isUpdated = productsRepository.updateProduct(Number(req.params.id), req.body.title)
-    if (isUpdated) {
-        const product = productsRepository.findProductById(Number(req.params.id))
-        res.send(product)
-    } else res.send(404)
-})
+productsRouter.put('/:id',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const isUpdated = productsRepository.updateProduct(Number(req.params.id), req.body.title)
+
+        if (isUpdated) {
+            const product = productsRepository.findProductById(Number(req.params.id))
+            res.send(product)
+        } else res.send(404)
+    })
