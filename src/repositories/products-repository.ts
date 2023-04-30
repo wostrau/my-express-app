daae5bd3ae245db86def1e3bcd45fc127a1cfdac
wostrau <a.ostrovtsov@gmail.com>
2023-04-30 22:52:17 +0200

    09 / EventLoop + Async/await - DONE

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -1,34 +1,38 @@
+export type ProductType = { id: number, title: string }
+
 const products = [
     {id: 1, title: 'tomato'},
     {id: 2, title: 'orange'}
 ]
 
 export const productsRepository = {
-    findProducts(title: string | null) {
+    async findProducts(title: string | null): Promise<ProductType[]> {
         if (title) {
             return products.filter(p => p.title.indexOf(title) > -1)
         } else return products
     },
-    findProductById(id: number) {
-        const searchedProduct = products.find(p => p.id === id)
-        return searchedProduct
+    findProductById(id: number): ProductType | null {
+        const foundProduct = products.find(p => p.id === id)
+        if (foundProduct) {
+            return foundProduct
+        } else return null
     },
-    createProduct(title: string) {
-        const newProduct = {
+    async createProduct(title: string): Promise<ProductType> {
+        const newProduct: ProductType = {
             id: Number(new Date()),
             title: title
         }
         products.push(newProduct)
         return newProduct
     },
-    updateProduct(id: number, title: string) {
+    async updateProduct(id: number, title: string): Promise<boolean> {
         const updatedProduct = products.find(p => p.id === id)
         if (updatedProduct) {
             updatedProduct.title = title
             return true
         } else return false
     },
-    deleteProduct(id: number) {
+    async deleteProduct(id: number): Promise<boolean> {
         for (let i = 0; i < products.length; i++) {
             if (products[i].id === id) {
                 products.splice(i, 1)
--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -1,7 +1,6 @@
 import {Request, Response, Router} from 'express'
-import {productsRepository} from '../repositories/products-repository'
-import {requestCounter} from '../index'
-import {body, validationResult} from 'express-validator'
+import {productsRepository, ProductType} from '../repositories/products-repository'
+import {body} from 'express-validator'
 import {inputValidationMiddleware} from '../middlewares/input-validation-middleware'
 
 export const productsRouter = Router({})
@@ -12,14 +11,20 @@ const titleValidation = body('title')
     .withMessage('title length should be from 3 to 10 symbols')
 
 
-productsRouter.get('/', (req: Request, res: Response) => {
+productsRouter.get('/', async (req: Request, res: Response) => {
     const title = typeof req.query.title === 'string'
         ? req.query.title?.toString()
         : null
 
-    const foundProducts = productsRepository.findProducts(title)
+    const foundProducts: ProductType[] = await productsRepository.findProducts(title)
 
-    if (foundProducts) res.send(foundProducts + requestCounter)
+    let start = performance.now()
+    while (performance.now() - start < 3000) {
+        console.log(performance.now() - start)
+    }
+
+
+    if (foundProducts) res.send(foundProducts)
     else res.send(404)
 })
 
@@ -30,8 +35,8 @@ productsRouter.get('/:id', (req: Request, res: Response) => {
     else res.send(404)
 })
 
-productsRouter.delete('/:id', (req: Request, res: Response) => {
-    const isDeleted = productsRepository.deleteProduct(Number(req.params.id))
+productsRouter.delete('/:id', async (req: Request, res: Response) => {
+    const isDeleted: boolean = await productsRepository.deleteProduct(Number(req.params.id))
 
     if (isDeleted) res.send(204)
     else res.send(404)
@@ -40,8 +45,8 @@ productsRouter.delete('/:id', (req: Request, res: Response) => {
 productsRouter.post('/',
     titleValidation,
     inputValidationMiddleware,
-    (req: Request, res: Response) => {
-        const newProduct = productsRepository.createProduct(req.body.title)
+    async (req: Request, res: Response) => {
+        const newProduct: ProductType = await productsRepository.createProduct(req.body.title)
 
         res.status(201).send(newProduct)
     })
@@ -49,8 +54,8 @@ productsRouter.post('/',
 productsRouter.put('/:id',
     titleValidation,
     inputValidationMiddleware,
-    (req: Request, res: Response) => {
-        const isUpdated = productsRepository.updateProduct(Number(req.params.id), req.body.title)
+    async (req: Request, res: Response) => {
+        const isUpdated: boolean = await productsRepository.updateProduct(Number(req.params.id), req.body.title)
 
         if (isUpdated) {
             const product = productsRepository.findProductById(Number(req.params.id))
